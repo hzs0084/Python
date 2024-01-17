afd94510477fa78eb62748f3df29db6f7b5cc84b
Hemant Sherawat <hzs0084@auburn.edu>
2024-01-17 16:54:18 -0600

    Fourth Commit

---
+++ Code & Debug/Basics/helloWorld.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+print("This is a test for a new line", end= " ") 
+# end = ; that will make sure that the output prints on the same line
+print("new Line?")
+
+#Single Line Comments
+
+"""
+Multi Line Comments
+"""
+
