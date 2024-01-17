4ffef12c32020cd9b8373c0a4625b9a4eae99137
Hemant Sherawat <hzs0084@auburn.edu>
2024-01-17 07:17:23 -0600

    Second commit

---
+++ Chef and Instant Noodles.py
@@ -0,0 +1,8 @@
+# cook your dish here
+x, y = [int(x) for x in input().split()] #Since the input is separated by spaces this works
+
+z = x * y
+
+print(z)
+
+# Another solution is using a map but then I need to look into what map is and how it works
